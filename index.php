089551654300593d83a92db45363d88a56fe507c
Antoine Cantoro <antoine.cantoro@gmail.com>
2024-11-14 20:24:42 +0100

    Setup architecture

---
+++ index.php
@@ -0,0 +1,11 @@
+<head>
+    <title>test</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<html>
+    <body>
+        <div class="container">
+          <h1>test</h1>
+        </div>
+    </body>
+</html>
